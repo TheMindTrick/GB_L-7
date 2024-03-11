cdd4c848364744ff4c30f3967a6d60681681eb14
Iskander M. <themindtrick@hotmail.com>
2024-03-11 23:38:11 +0500

    task_1+2+3

--- task-3/Program.cs
+++ task-3/Program.cs
@@ -5,17 +5,15 @@
 
 void Main()
 {
-    int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+    int[] numbers = { 11, 22, 33, 44, 55, 66, 77, 88, 99 };
 
     PrintNumbers(numbers, 0);
 }
 
 void PrintNumbers(int[] numbers, int index)
 {
-    if (numbers[index] == numbers.Length - 1) return;
-
+    if (index == numbers.Length) return;
     PrintNumbers(numbers, index + 1);
-
     System.Console.Write(numbers[index] + " ");
 }
 
