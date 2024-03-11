7a08a7cd6c7f70d94728d8b3b57e78696d69a650
Iskander M. <themindtrick@hotmail.com>
2024-03-11 23:29:36 +0500

    task-1+2+3

--- task-3/Program.cs
+++ task-3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World333!");
+﻿/* ############################ Задача №3 ############################
+    Задача 3: Задайте произвольный массив. Выведете его элементы,
+    начиная с конца. Использовать рекурсию, не использовать циклы.
+################################################################### */
+
+void Main()
+{
+    int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+    PrintNumbers(numbers, 0);
+}
+
+void PrintNumbers(int[] numbers, int index)
+{
+    if (numbers[index] == numbers.Length - 1) return;
+
+    PrintNumbers(numbers, index + 1);
+
+    System.Console.Write(numbers[index] + " ");
+}
+
+Main();
